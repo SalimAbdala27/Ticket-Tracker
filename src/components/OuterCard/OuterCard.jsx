84a283f4eb1ba412acea36679e558c5ebe79a41f
Isher Sagay <ishersagay@gmail.com>
2021-09-14 15:58:45 +0100

    managed to pull the data from team.js and plug it into OuterCard.jsx and display it

--- src/components/OuterCard/OuterCard.jsx
+++ src/components/OuterCard/OuterCard.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import "./OuterCard.scss"
 
-const OuterCard = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+const OuterCard = (props) => {
+
+    const  { name, role} = props;
+
+
+    return (
+        <>
+            <div>
+                <p>{name} </p>
+                <p>{role} </p>
+            </div>
+        </>
+    )
 }
 
 export default OuterCard
