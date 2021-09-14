d47dec7eee0a5118bc0db30c3570af41955ee0b9
SalimAbdala27 <salimabdala27@hotmail.com>
2021-09-14 14:58:20 +0100

    components created

---
+++ src/components/InnerCard/InnerCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./InnerCard.scss"
+
+const InnerCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default InnerCard
---
+++ src/components/OuterCard/OuterCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./OuterCard.scss"
+
+const OuterCard = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default OuterCard
